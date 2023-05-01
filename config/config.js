353c33afba637999adf5755579ea68558df3b452
Pita Badila <ritualmomma@gmail.com>
2023-05-01 12:10:57 -0400

    corrected config.js

--- config/config.js
+++ config/config.js
@@ -18,7 +18,7 @@ module.exports =
   },
   "production": {
     "username": process.env.DATABASE_USER,
-    "password": process.env.DDATABASE_PASSWORD,
+    "password": process.env.DATABASE_PASSWORD,
     "database": process.env.DATABASE_NAME,
     "host": process.env.DATABASE_HOST,
     "dialect": "postgres"
